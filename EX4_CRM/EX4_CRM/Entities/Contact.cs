dc986368c8cd6588eb273a585a9f241942a1c4a8
NinPhi <melmarka4@gmail.com>
2024-04-04 13:31:24 +0600

    added contact entity

---
+++ EX4_CRM/EX4_CRM/Entities/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EX4_CRM.Entities;
+
+public class Contact
+{
+    public long Id { get; set; }
+
+    public required long MarketerId { get; set; }
+
+    [StringLength(100)]
+    public required string FirstName { get; set; }
+
+    [StringLength(150)]
+    public string? MiddleName { get; set; }
+
+    [StringLength(150)]
+    public string? LastName { get; set; }
+
+    [StringLength(50)]
+    public required string Phone { get; set; }
+
+    [StringLength(200)]
+    public string? Email { get; set; }
+
+    public required string ContactStatus { get; set; }
+
+    public User? Marketer { get; set; }
+}
