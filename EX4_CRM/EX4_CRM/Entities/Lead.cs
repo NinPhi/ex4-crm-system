9868fb58e6fd2d84a5bfb9c7876683d693ff56b2
NinPhi <melmarka4@gmail.com>
2024-04-04 13:33:58 +0600

    added lead entity

---
+++ EX4_CRM/EX4_CRM/Entities/Lead.cs
@@ -0,0 +1,16 @@
+﻿namespace EX4_CRM.Entities;
+
+public class Lead
+{
+    public long Id { get; set; }
+
+    public required long ContactId { get; set; }
+
+    public required long SalesmanId { get; set; }
+
+    public required string LeadStatus { get; set; }
+
+    public Contact? Contact { get; set; }
+
+    public User? Salesman { get; set; }
+}
