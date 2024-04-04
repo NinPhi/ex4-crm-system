00f9cbedf5baf3dc0d6719e80f0b0c98aebe9a6d
NinPhi <melmarka4@gmail.com>
2024-04-04 13:27:40 +0600

    added user entity

---
+++ EX4_CRM/EX4_CRM/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EX4_CRM.Entities;
+
+public class User
+{
+    public long Id { get; set; }
+
+    [StringLength(300)]
+    public string? FullName { get; set; }
+
+    [StringLength(200)]
+    public required string Email { get; set; }
+
+    [StringLength(200)]
+    public required string PasswordHash { get; set; }
+
+    public required string Role { get; set; }
+
+    public DateTime? BlockedOn { get; set; }
+}
