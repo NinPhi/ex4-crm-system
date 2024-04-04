1d1ffde13497b53c4fa3065a4c3236cc21080fe7
NinPhi <melmarka4@gmail.com>
2024-04-04 13:36:02 +0600

    added sale entity

---
+++ EX4_CRM/EX4_CRM/Entities/Sale.cs
@@ -0,0 +1,16 @@
+﻿namespace EX4_CRM.Entities;
+
+public class Sale
+{
+    public long Id { get; set; }
+
+    public required long LeadId { get; set; }
+
+    public required long SalesmanId { get; set; }
+
+    public required DateTime SoldOn { get; set; }
+
+    public Lead? Lead { get; set; }
+
+    public User? Salesman { get; set; }
+}
