62b9b72d5166962b906bd201f0469870bcd93db4
NinPhi <melmarka4@gmail.com>
2024-04-04 13:50:56 +0600

    created and configured dbcontext

---
+++ EX4_CRM/EX4_CRM/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using EX4_CRM.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EX4_CRM.Data;
+
+public class AppDbContext(
+    DbContextOptions<AppDbContext> opts) : DbContext(opts)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Lead>()
+            .HasOne(s => s.Salesman)
+            .WithMany()
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder
+            .Entity<Sale>()
+            .HasOne(s => s.Salesman)
+            .WithMany()
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
